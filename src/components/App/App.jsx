12ec1863398cb33074e6c9021907ab2df6f91415
Andriana3 <andrianavovchok@gmail.com>
2024-01-25 17:00:52 +0100

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,64 +1,60 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Layout } from './App.styled';
-import { Section, FeedbackOptions, Statistics, Notification } from 'components';
+import { Section, Statistics, Notification } from 'components';
+import { FeedbackOptions } from 'components/FeedbackOpions/FeedbackOptions';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [totalFeedback, setTotalFeedback] = useState(0);
+  const [feedbackPercentage, setFeedbackPercentage] = useState(0);
 
-  totalFeedback = 0;
-  positiveFeedbackPercentage = 0;
-
-  addFeedback = e => {
+  const addFeedback = e => {
     const { id } = e.target;
-    this.setState(prevState => {
-      return { [id]: prevState[id] + 1 };
-    });
+    switch (id) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        throw new Error('Unsupported feedback option');
+    }
   };
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    this.totalFeedback = good + neutral + bad;
-  };
+  useEffect(() => {
+    const total = good + neutral + bad;
+    setTotalFeedback(() => total);
+    setFeedbackPercentage(() => Number(((good / total) * 100).toFixed(0)));
+  }, [bad, good, neutral]);
 
-  countPositiveFeedbackPercentage = () => {
-    this.positiveFeedbackPercentage = Number(
-      ((this.state.good / this.totalFeedback) * 100).toFixed(0)
-    );
-  };
+  return (
+    <Layout>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          addFeedback={addFeedback}
+        ></FeedbackOptions>
+      </Section>
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    this.countTotalFeedback();
-    this.countPositiveFeedbackPercentage();
-    return (
-      <Layout>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.addFeedback}
-          ></FeedbackOptions>
-        </Section>
-
-        <Section title="Statistics">
-          {this.totalFeedback ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.totalFeedback}
-              positivePercentage={this.positiveFeedbackPercentage}
-            ></Statistics>
-          ) : (
-            <Notification message="There is no feedback"></Notification>
-          )}
-        </Section>
-      </Layout>
-    );
-  }
+      <Section title="Statistics">
+        {totalFeedback ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePercentage={feedbackPercentage}
+          ></Statistics>
+        ) : (
+          <Notification message="There is no feedback"></Notification>
+        )}
+      </Section>
+    </Layout>
+  );
 }
-
-export { App };
--- src/components/FeedbackOpions/FeedbackOptions.jsx
+++ src/components/FeedbackOpions/FeedbackOptions.jsx
@@ -2,23 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Option, OptionList } from './FeedbackOptions.styled';
 
-const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+export function FeedbackOptions({ options, addFeedback }) {
   return (
     <OptionList>
       {options.map(option => {
         return (
-          <Option key={option} id={option} onClick={onLeaveFeedback}>
+          <Option key={option} id={option} onClick={addFeedback}>
             {option}
           </Option>
         );
       })}
     </OptionList>
   );
-};
-
-export { FeedbackOptions };
+}
 
 FeedbackOptions.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string).isRequired,
-  onLeaveFeedback: PropTypes.func.isRequired,
+  addFeedback: PropTypes.func.isRequired,
 };
