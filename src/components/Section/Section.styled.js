c28555f2abd6f5a2c0cc48e57918a4de3611ad7c
Andriana3 <andrianavovchok@gmail.com>
2024-01-25 16:43:36 +0100

    update

---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const SectionWrap = styled.div`
+  width: 400px;
+  padding: 20px 20px 0 20px;
+  text-align: center;
+  background-color: #ffc0cb;
+`;
+
+export const Title = styled.h2`
+  font-size: 32px;
+  margin: 0;
+`;
---
+++ src/dataBase/optionColors.js
@@ -0,0 +1,5 @@
+export const optionColors = {
+  good: '#ACE1AF',
+  neutral: '#FBCEB1',
+  bad: '#fd5c63',
+};
